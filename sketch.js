b8576c0f82593d599975700bf2e277b9ba089bae
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-25 10:19:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,61 +1,45 @@
-let operator,input_num1, input_num2; 
-let greeting, greeting2, greeting3;
-let num1, num2;
+let greeting, num1, score;
 
 function setup() {
   
   createCanvas(400, 200);
   background(178,255,102);
   
-  input_num1 = createInput();
-  input_num1.position(5, 60);
+ num1 = createInput();
+ num1.position(5, 60);
   greeting = createElement('h5', 'Enter number 1:');
   greeting.position(5, 20);
   
-  input_num2 = createInput();
-  input_num2.position(200, 60);
-  greeting2 = createElement('h5', 'Enter operator:');
-  greeting2.position(120, 80);
-
-  operator = createInput();
-  operator.position(100, 120);
-  greeting3 = createElement('h5', 'Enter number 2:');
-  greeting3.position(200, 20);
-  
+ 
   textAlign(CENTER);
   textSize(50);
 }
 
 function draw() {
   
-  num1 = input_num1.value();
-  num2 = input_num2.value();
-  var opr = operator.value();
+ score= num1.value();
+ 
   
-  switch (opr) {
+  switch (true) {
       
-    case '+':
-      console.log("Addition of num1 & num2: ");
-      console.log(int(num1) + int(num2));
+    case(score<=100 && score>=90):
+      console.log("A");
       break;
       
-    case '-':
-      console.log("Subtraction of num1 & num2: ");
-      console.log(int(num1) - int(num2));
+     case(score<=89 && score>=80):
+      console.log("B");
       break;
-      
-    case '*':
-      console.log("Multiplication of num1 & num2: ");
-      console.log(int(num1) * int(num2));
+     case(score<=79 && score>=70):
+      console.log("c");
       break;
-      
-    case '/':
-      console.log("Division of num1 & num2: ");
-      console.log(int(num1) / int(num2));  
+       case(score<=69 && score>=60):
+      console.log("D");
       break;
-    
+       case(score<=59 && score>=0):
+      console.log("F");
+      break;
     default:
-      console.log("Please enter Valid operator."); 
+      console.log("INVALID SCORE."); 
   }
 }
 
